dc07953e5d5e80a49275b49922c6d2f33284dd67
Naeim <naeim.smz@gmail.com>
2025-05-20 01:52:31 +0430

    adding some css module and app futures

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,15 @@
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import PageNav from "../components/PageNav";
+import AppNav from "../components/AppNav";
 
 function HomePage() {
   return (
     <div>
+      <PageNav />
+      <AppNav />
       <h1>HomePage</h1>
 
-      <PageNav />
-      {/* <Link to="/pricing">Pricing</Link> */}
+      <Link to="/app">Go to the app</Link>
     </div>
   );
 }
