bfe1feaab0f3edd8e665620a03f561967498365d
Naeim <naeim.smz@gmail.com>
2025-05-19 20:00:54 +0430

    linking homepage with pricing component with adding link component from react-router-dom

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,13 @@
+import { Link } from "react-router-dom";
+
 function HomePage() {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <h1>HomePage</h1>
+
+      <Link to="/pricing">Pricing</Link>
+    </div>
+  );
 }
 
 export default HomePage;
