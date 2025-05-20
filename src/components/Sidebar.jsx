54099950a4256615d5f09940dead7124db574f48
Naeim <naeim.smz@gmail.com>
2025-05-20 18:13:05 +0430

    creating map , sidebarand footer components

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,19 @@
+import Logo from "./Logo";
+import AppNav from "./AppNav";
+import styles from "./Sidebar.module.css";
+import Footer from "./Footer";
+
+function Sidebar() {
+  return (
+    <div className={styles.Sidebar}>
+      <Logo />
+      <AppNav />
+
+      <p>List of cities</p>
+
+      <Footer />
+    </div>
+  );
+}
+
+export default Sidebar;
