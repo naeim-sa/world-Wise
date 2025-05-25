bf168c8ae58dab06a5884965e7b6471c3bb3431c
Naeim <naeim.smz@gmail.com>
2025-05-25 19:20:51 +0430

    Programmatic Navigation with Navigate

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { CitiesProvider } from "./context/CitiesContext";
 
 import HomePage from "./pages/HomePage";
@@ -22,7 +22,7 @@ function App() {
           <Route path="pricing" element={<Pricing />} />
           <Route path="login" element={<Login />} />
           <Route path="app" element={<AppLayout />}>
-            <Route index element={<CityList />} />
+            <Route index element={<Navigate replace to="cities" />} />
             <Route path="cities" element={<CityList />} />
             <Route path="cities/:di" element={<City />} />
             <Route path="countries" element={<CountryList />} />
