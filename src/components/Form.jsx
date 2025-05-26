c6c3ac6841897dab0c0e3ec54046e542c9637d61
Naeim <naeim.smz@gmail.com>
2025-05-26 18:54:22 +0430

    create a city object to add the city that select on the map by the form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -11,6 +11,7 @@ import Button from "./Button";
 import BackButton from "./BackButton";
 import Message from "../components/Message";
 import Spinner from "../components/Spinner";
+import { useCities } from "../context/CitiesContext";
 
 export function convertToEmoji(countryCode) {
   const codePoints = countryCode
@@ -24,6 +25,7 @@ const BASE_URL = "https://api.bigdatacloud.net/data/reverse-geocode-client";
 
 function Form() {
   const [lat, lng] = useUrlPosition();
+  const { createCity } = useCities();
 
   const [cityName, setCityName] = useState("");
   const [isLoadingGeocoding, setIsLoadingGeocoding] = useState(false);
@@ -68,6 +70,19 @@ function Form() {
 
   function handleSubmit(e) {
     e.preventDefault();
+
+    if (!cityName || !date) return;
+
+    const newCity = {
+      cityName,
+      country,
+      emoji,
+      date,
+      notes,
+      position: { lat, lng },
+    };
+
+    createCity(newCity);
   }
 
   if (isLoadingGeocoding) return <Spinner />;
@@ -91,13 +106,13 @@ function Form() {
 
       <div className={styles.row}>
         <label htmlFor="date">When did you go to {cityName}?</label>
-        {/* <input
-          id="date"
-          onChange={(e) => setDate(e.target.value)}
-          value={date}
-        /> */}
 
-        <DatePicker />
+        <DatePicker
+          id="date"
+          onChange={(date) => setDate(date)}
+          selected={date}
+          dateFormat="dd/MM/yyyy"
+        />
       </div>
 
       <div className={styles.row}>
