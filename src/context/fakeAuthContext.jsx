b359d32d98dc162d6b2e887d2a9ac7292875ef02
Naeim <naeim.smz@gmail.com>
2025-05-28 17:08:24 +0430

    implemented logout feuture

--- src/context/fakeAuthContext.jsx
+++ src/context/fakeAuthContext.jsx
@@ -19,7 +19,7 @@ function reducer(state, action) {
 }
 
 const FAKE_USER = {
-  name: "Jack",
+  name: "Naeim Sa",
   email: "jack@example.com",
   password: "qwerty",
   avatar: "https://i.pravatar.cc/100?u=zz",
